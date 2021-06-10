a850aad17818a83563856f5c797b8c957c28cd78
Raleigh.j55@gmail.com <r.aleigh.j55@gmail.com>
2021-06-10 11:57:28 -0500

    Updated Java

---
+++ JS/Javascript.js
@@ -0,0 +1,46 @@
+alert("Hello, World");          //Alert before webpage loads
+window.alert("Hello, again.")
+document.write("Hello, once again")
+var X = "Hello, world."             ///Last alerts
+document.write (X);
+var A = "This is a string";         //String 
+document.write (A);
+document.write (A) + (X);
+const newString1 = "This is a string assigned to a variable.";
+console.log(newString1);
+function my_First_Function() {          //funtion
+    text1 ="This is text" ;
+    text1 =text1.fontcolor("green") ;
+    document.getElementById("result2").innerHTML = text1 ;
+}
+var B = "Concatenated" + "String" ;         
+document.write (B) ;    
+const newString = ("Lisa told Bart, \"Knock it off, Bart! Or I'll tell Dad!\"Eat my shorts! \" Bart responded.") ;
+window.alert = (newString);
+function displayDate() {
+    document.getElementById("result1").innerHTML = Date();
+  }
+  let myPet = 'seahorse';
+  console.log('My favorite animal is the ' + myPet + '.');   // My favorite animal is the seahorse.         ```
+  function greeting() {
+    greeting =greeting.fontcolor("red");
+  }
+  let a = 2;
+let b = 'hello';
+
+console.log(a += 3); // addition. expected output: 5 jeff
+
+console.log(b += ' world'); // concatenation
+// expected output: "hello world"
+document.write(a) + (b);
+
+function mult1(p1, p2) {
+    return p1 * p2 ;
+}
+document.getElementById("mult1").innerHTML=(p1, p2);
+
+
+function buttontext() {
+  document.getElementById("buttontext").innerHTML = "Hello World";
+}
+
