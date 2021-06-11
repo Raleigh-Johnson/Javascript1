8c56a3a70324bf95595e91a1438f9adbe5829265
Raleigh.j55@gmail.com <r.aleigh.j55@gmail.com>
2021-06-11 14:18:02 -0500

    updated Operations

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,32 @@
  // Math OPERATORS
- function addition_Funtion() {
-    var addition = 2+2;
-    document.getElementById("Math").innerHTML = "2 + 2 =" + addition;
-  }
+
+
+function addition() {
+var addition = 2 + 2;
+document.getElementById("addition") .innerHTML = "2 + 2 =" + addition;
+}
   
-  function subtraction_Funtion() {
-    var Subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 =" + subtraction;
-  }
+function subtraction() {
+    var simple_Math = 5 - 2;
+    document.getElementById("subtraction") .innerHTML = "5 - 2 =" + simple_Math;
+    }
 
   function multiplication() {
     var simple_Math = 6 * 8;
-    document.getElementById("Math").innerHTML = "6 * 8 =" + simple_Math;
+    document.getElementById("multiplication").innerHTML = "6 * 8 = " + simple_Math;
   }
 
   function division() {
     var simple_Math = 48 / 6;
-    document.getElementById("Math").innerHTML = "48 / 6 =" + simple_Math;
+    document.getElementById("division").innerHTML = "48 / 6 =" + simple_Math;
   }
-  document.write = division;
-  function more_Math()  {
-      var simple_Math = (1+2) * 10 / 2 -5;
-      document.getElementById("Math").innerHTML = "1 plus 2 multiplied by 10 devided by 2 and subtract 5 = " + simple_Math;
-      return simple_Math;
+  function math()  {
+      var math = (1+2) * 10 / 2 -5;
+      document.getElementById("math").innerHTML = "1 plus 2 multiplied by 10 devided by 2 and subtract 5 = " + math;
   }
   function modulus_Operator() {
     var simple_Math = 25 % 6;
-    document.getElementById("Math").innerHTML = "When you devide 25 by 6, you hve a remainder of: " + simple_Math;
+    document.getElementById("Math5").innerHTML = "When you devide 25 by 6, you hve a remainder of: " + simple_Math;
 }
+
   
