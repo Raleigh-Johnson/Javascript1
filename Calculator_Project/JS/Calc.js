bea3b64793ead256ee1de8212036a972a31059a7
Raleigh.j55@gmail.com <r.aleigh.j55@gmail.com>
2021-06-21 16:19:54 -0500

    Updated Calc and Pizza Code Working

--- Calculator_Project/JS/Calc.js
+++ Calculator_Project/JS/Calc.js
@@ -11,19 +11,19 @@ function Input_Digit(digit) {
         Calculator.Display_Value = digit;
         Calculator.Wait_Second_Operand = false;
     } else {
-        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit
+        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
     }
 }
 
 function Input_Decimal (dot) {
-    if (Calculator.Wait_Second_Operand === true) return;
-    if(! Calculator.Display_Value.includes(dot)) {
+    if (Calculator.Wait_Second_Operand === 'true') return;
+    if(!Calculator.Display_Value.includes(dot)) {
         Calculator.Display_Value += dot;
     }
 }
 
 function Handle_Operator(Next_Operator) {
-    const { First_Operand, Display_Value, operator } = Calculator
+    const { First_Operand, Display_Value, operator } = Calculator;
     const Value_of_Input = parseFloat(Display_Value);
     if(operator && Calculator.Wait_Second_Operand) {
         Calculator.operator = Next_Operator;
@@ -35,17 +35,19 @@ function Handle_Operator(Next_Operator) {
             const Value_Now = First_Operand || 0;
             let result = Perform_Calculation[operator](Value_Now, Value_of_Input);
             result = Number(result).toFixed(9)
-            result = Number(result).toString()
-            Calculator.Display_Value = parseFloat(result);
-            Calculator.FirstOperand = parseFloat(result);
+            result = (result * 1).toString()
+            Calculator.Display_Value = result;
+            Calculator.First_Operand = result;
        }
        Calculator.Wait_Second_Operand = true;
        Calculator.operator = Next_Operator;
 }
 
+
 const Perform_Calculation = {
     '/':(First_Operand, Second_Operand) => First_Operand / Second_Operand,
     '*':(First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    '+':(First_Operand, Second_Operand) => First_Operand + Second_Operand,
     '-':(First_Operand, Second_Operand) => First_Operand - Second_Operand,
     '=':(First_Operand, Second_Operand) => Second_Operand
 };
