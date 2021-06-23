10bf550e6e47da4d8b66caeb7a8391f9a1d16a7d
Raleigh.j55@gmail.com <r.aleigh.j55@gmail.com>
2021-06-23 11:34:49 -0500

    UPDATED PIZZA

--- Pizza_Project/JS/Pizza.js
+++ Pizza_Project/JS/Pizza.js
@@ -1,14 +1,15 @@
-function getReceipt() {
+function getReceipt() { //Function to total the orderr's total
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
-    var sizeArray = document.getElementsByClassName("size");
+    var sizeArray = document.getElementsByClassName("size"); //Selector for each size
     for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
             text1 = text1+selectedSize+ "<br>"; 
         }
-    }
+    } 
+    //Each cost for each Size of Pizza
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Medium Pizza") {
@@ -18,14 +19,14 @@ function getReceipt() {
     } else if (selectedSize === "Extra Large") {
         sizeTotal = 12;
     }
-    runningTotal = sizeTotal;
+    runningTotal = sizeTotal; // Running total without toppings and all of that
     console.log(selectedSize+ " = $"+sizeTotal+".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $"+ runningTotal+".00");
     getTopping(runningTotal,text1);
-};
+}; 
 
-function getTopping(runningTotal,text1) {
+function getTopping(runningTotal,text1) { //Output of Size + Toppings
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
@@ -37,7 +38,7 @@ function getTopping(runningTotal,text1) {
 
         } 
     }
-    var toppingCount =  selectedTopping.length;
+    var toppingCount =  selectedTopping.length; //One free topping, after one, Each adds 1$ 
     if (toppingCount > 1) {
         toppingTotal = ( toppingCount - 1);
     } else  {
@@ -46,7 +47,7 @@ function getTopping(runningTotal,text1) {
 
 runningTotal = (runningTotal + toppingTotal);
 console.log("total selected topping items"+ toppingCount);
-console.log(toppingCount+ "topping - 1 free topping = "+"$"+toppingTotal+".00");
+console.log(toppingCount+ "topping - 1 free topping = "+"$"+toppingTotal+".00"); //Further toppings
 console.log("topping text1:"+text1);
 console.log("Purchase Total"+"$"+runningTotal+".00");
 document.getElementById("showText").innerHTML = text1;
